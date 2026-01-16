e39baa13faf7f9f0604e945e6c923e02b335eb9d
chaminduchirantha <chaminduchirantha73@gmail.com>
2026-01-16 18:43:24 +0530

    updateregister page convert the light mode to dark mode

--- app/(auth)/register.tsx
+++ app/(auth)/register.tsx
@@ -8,7 +8,8 @@ import {
   Platform, 
   ScrollView, 
   StatusBar, 
-  Alert 
+  Alert, 
+  useColorScheme
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { useRouter } from "expo-router";
@@ -21,6 +22,10 @@ export default function RegisterScreen() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [confirmPassword, setConfirmPassword] = useState("");
+  const systemTheme = useColorScheme();
+  const [theme, setTheme] = useState(systemTheme || "light");
+  
+  const isDark = theme === "dark";
 
   const handleRegister = () => {
     if (!name || !email || !password || !confirmPassword) {
@@ -37,8 +42,8 @@ export default function RegisterScreen() {
   };
 
   return (
-    <SafeAreaView className="flex-1 bg-slate-100">
-      <StatusBar barStyle="dark-content" />
+    <SafeAreaView className={`flex-1 ${isDark ? "bg-slate-900" : "bg-slate-100"}`}>
+      <StatusBar barStyle={isDark ? "light-content" : "dark-content"} />
 
       <KeyboardAvoidingView
         behavior={Platform.OS === "ios" ? "padding" : "height"}
@@ -51,11 +56,22 @@ export default function RegisterScreen() {
         >
 
           <View className="px-6 pt-10 pb-8 items-center">
+            <TouchableOpacity
+              onPress={() => setTheme(isDark ? "light" : "dark")}
+              className="absolute right-6 top-6"
+            >
+              <Ionicons
+                name={isDark ? "sunny" : "moon"}
+                size={26}
+                color={isDark ? "#99a1af" : "#99a1af"}
+                style={{ opacity: 0.85 }}
+              />
+            </TouchableOpacity>
             <View className="w-16 h-16 rounded-2xl bg-teal-600 items-center justify-center mb-5 shadow-lg shadow-teal-600/50">
               <Text className="text-3xl text-white">✍️</Text>
             </View>
 
-            <Text className="text-3xl font-extrabold text-center text-slate-900 mb-1">
+            <Text className={`text-3xl font-extrabold text-center mb-5 ${isDark? " text-white": "text-black"}`}>
               Create Account
             </Text>
             <Text className="text-base text-slate-500 text-center px-4">
@@ -63,7 +79,7 @@ export default function RegisterScreen() {
             </Text>
           </View>
 
-          <View className="flex-1 bg-white rounded-[20px] m-5 px-4 pt-10 shadow-2xl shadow-black">
+          <View className={`flex-1 rounded-[20px] m-5 px-4 pt-10 shadow-2xl ${isDark ? "bg-slate-800" : "bg-white"}`}>
             <View className="mb-5">
               <Text className="text-xs font-bold text-slate-600 mb-2 ml-1 tracking-widest">
                 FULL NAME
@@ -74,7 +90,11 @@ export default function RegisterScreen() {
                 onChangeText={setName}
                 autoCapitalize="words"
                 placeholderTextColor="#94a3b8"
-                className="bg-slate-50 rounded-2xl border border-slate-200 p-4 text-slate-900 text-base"
+                className={`rounded-2xl border p-4 text-base ${
+                  isDark
+                    ? "bg-slate-700 border-slate-600 text-white"
+                    : "bg-slate-50 border-slate-200 text-slate-900"
+                }`}
               />
             </View>
 
@@ -89,7 +109,11 @@ export default function RegisterScreen() {
                 keyboardType="email-address"
                 autoCapitalize="none"
                 placeholderTextColor="#94a3b8"
-                className="bg-slate-50 rounded-2xl border border-slate-200 p-4 text-slate-900 text-base"
+                className={`rounded-2xl border p-4 text-base ${
+                  isDark
+                    ? "bg-slate-700 border-slate-600 text-white"
+                    : "bg-slate-50 border-slate-200 text-slate-900"
+                }`}
               />
             </View>
 
@@ -103,7 +127,11 @@ export default function RegisterScreen() {
                 onChangeText={setPassword}
                 secureTextEntry
                 placeholderTextColor="#94a3b8"
-                className="bg-slate-50 rounded-2xl border border-slate-200 p-4 text-slate-900 text-base"
+                className={`rounded-2xl border p-4 text-base ${
+                  isDark
+                    ? "bg-slate-700 border-slate-600 text-white"
+                    : "bg-slate-50 border-slate-200 text-slate-900"
+                }`}
               />
             </View>
 
@@ -117,7 +145,11 @@ export default function RegisterScreen() {
                 onChangeText={setConfirmPassword}
                 secureTextEntry
                 placeholderTextColor="#94a3b8"
-                className="bg-slate-50 rounded-2xl border border-slate-200 p-4 text-slate-900 text-base"
+                className={`rounded-2xl border p-4 text-base ${
+                  isDark
+                    ? "bg-slate-700 border-slate-600 text-white"
+                    : "bg-slate-50 border-slate-200 text-slate-900"
+                }`}
               />
             </View>
 
@@ -136,10 +168,28 @@ export default function RegisterScreen() {
             </View>
 
             <TouchableOpacity
-              className="bg-white py-4 rounded-2xl border border-slate-200 flex-row items-center justify-center mb-6"
+              // onPress={}
+              activeOpacity={0.85}
+              className={`rounded-2xl py-4 border flex-row items-center justify-center mb-6 ${
+                isDark
+                  ? "bg-slate-700 border-slate-600"
+                  : "bg-slate-50 border-slate-200"
+              }`}
             >
-              <Text className="text-xl mr-3">🔍</Text>
-              <Text className="text-slate-700 font-bold text-base">Continue with Google</Text>
+              <AntDesign
+                name="google"
+                size={20}
+                color="#00bba7"
+                style={{ marginRight: 12 }}
+              />
+
+              <Text
+                className={`font-bold text-base ${
+                  isDark ? "text-white" : "text-slate-900"
+                }`}
+              >
+                Continue with Google
+              </Text>
             </TouchableOpacity>
 
             <View className="flex-row justify-center pb-10">
